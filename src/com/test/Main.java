5056a1fa7aa1ae946e557e398389b6fa251ee816
biubiubiu123321 <1914417081@qq.com>
2017-03-28 10:11:44 +0800

    Update Main.java

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -5,5 +5,8 @@ public class Main {
 	public static void main (String[] arg){
 		System.out.println("this is a test");
 		System.out.println("cdf");
+		
+		test1 test = new test1();
+		test.sbxyl();
 	}
 }
